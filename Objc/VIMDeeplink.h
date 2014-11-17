b9a7354d4e6f4846ccff442f3f754e7bcba7f9d9
Alfie Hanssen <alfiehanssen@gmail.com>
2014-11-17 12:43:27 -0500

    rename, fix for swift/xcode evolution since initial commit

---
+++ Objc/VIMDeeplink.h
@@ -0,0 +1,23 @@
+//
+//  VIMDeeplink.h
+//  Vimeo
+//
+//  Created by Alfred Hanssen on 9/10/14.
+//  Copyright (c) 2014 Vimeo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VIMDeeplink : NSObject
+
++ (BOOL)viewVimeoAppInAppStore;
+
++ (BOOL)isVimeoAppInstalled;
+
++ (BOOL)openVimeoApp;
+
++ (BOOL)showVideoWithURI:(NSString *)videoURI;
+
++ (BOOL)showUserWithURI:(NSString *)userURI;
+
+@end
---
+++ Swift/VIMDeeplink.swift
@@ -0,0 +1,96 @@
+//
+//  VIMDeeplink.swift
+//  Vimeo
+//
+//  Created by Alfred Hanssen on 9/10/14.
+//  Copyright (c) 2014 Vimeo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+private let BaseURLString = "vimeo://app.vimeo.com"
+private let AppStoreURLString = "itms-apps://itunes.apple.com/us/app/425194759"
+
+private enum VimeoApp
+{
+    case Video(String)
+    case User(String)
+}
+
+private protocol URL
+{
+    var URL : NSURL { get }
+}
+
+extension VimeoApp : URL
+{
+    var URL: NSURL
+    {
+        switch self
+        {
+            case .Video(let videoURI): return NSURL(string: BaseURLString + videoURI)!
+            case .User(let userURI): return NSURL(string: BaseURLString + userURI)!
+        }
+    }
+}
+
+public class VimeoAppClient
+{
+    public class func viewVimeoAppInAppStore() -> Bool
+    {
+        if let URL = NSURL(string: AppStoreURLString)
+        {
+            return UIApplication.sharedApplication().openURL(URL)
+        }
+        
+        return false
+    }
+
+    public class func isVimeoAppInstalled() -> Bool
+    {
+        if let URL = NSURL(string: BaseURLString)
+        {
+            return UIApplication.sharedApplication().canOpenURL(URL)
+        }
+
+        return false
+    }
+
+    public class func openVimeoApp() -> Bool
+    {
+        if let URL = NSURL(string: BaseURLString)
+        {
+            return UIApplication.sharedApplication().openURL(URL)
+        }
+        
+        return false
+    }
+    
+    public class func showVideo(videoURI: String!) -> Bool
+    {
+        if (isVimeoAppInstalled())
+        {
+            let endpoint = VimeoApp.Video(videoURI)
+            let URL = endpoint.URL
+            
+            return UIApplication.sharedApplication().openURL(URL)
+        }
+        
+        return false
+    }
+
+    public class func showUser(userURI: String!) -> Bool
+    {
+        if (isVimeoAppInstalled())
+        {
+            let endpoint = VimeoApp.User(userURI)
+            let URL = endpoint.URL
+            
+            return UIApplication.sharedApplication().openURL(URL)
+        }
+        
+        return false
+    }
+
+}
