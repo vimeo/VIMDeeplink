881139c1813a9d6af12f71116c907e70734bd1c2
Rob Huebner <huebnerob@gmail.com>
2016-02-17 12:11:57 -0500

    update swift vimdeeplink

--- Swift/VIMDeeplink.swift
+++ Swift/VIMDeeplink.swift
@@ -14,13 +14,24 @@ private let AppStoreURLString = "itms-apps://itunes.apple.com/us/app/id425194759
 
 private let uploadLink = "/upload"
 private let profileLink = "/me"
+private let categoryLink = "/categories"
+private let exploreLink = "/explore"
+private let feedLink = "/feed"
+private let playlistsLink = "/playlists"
 
 private enum VimeoApp
 {
     case Video(String)
     case User(String)
-    case Upload
+    case AllCategories
+    case Category(String)
+    case Channel(String)
+    case Explore
+    case Feed
     case Me
+    case Playlists
+    case Upload
+    
 }
 
 private protocol URL
@@ -34,10 +45,16 @@ extension VimeoApp : URL
     {
         switch self
         {
-            case .Video(let videoURI): return NSURL(string: BaseURLString + videoURI)!
-            case .User(let userURI): return NSURL(string: BaseURLString + userURI)!
-            case .Upload: return NSURL(string: BaseURLString + uploadLink)!
-            case .Me: return NSURL(string: BaseURLString + profileLink)!
+        case .Video(let videoURI): return NSURL(string: BaseURLString + videoURI)!
+        case .User(let userURI): return NSURL(string: BaseURLString + userURI)!
+        case .AllCategories: return NSURL(string: BaseURLString + categoryLink)!
+        case .Category(let categoryURI): return NSURL(string: BaseURLString + categoryURI)!
+        case .Channel(let channelURI): return NSURL(string: BaseURLString + channelURI)!
+        case .Explore: return NSURL(string: BaseURLString + exploreLink)!
+        case .Feed: return NSURL(string: BaseURLString + feedLink)!
+        case .Me: return NSURL(string: BaseURLString + profileLink)!
+        case .Playlists: return NSURL(string: BaseURLString + playlistsLink)!
+        case .Upload: return NSURL(string: BaseURLString + uploadLink)!
         }
     }
 }
@@ -100,11 +117,11 @@ public class VimeoAppClient
         return false
     }
     
-    public class func showUpload() -> Bool
+    public class func showCategory(categoryURI: String!) -> Bool
     {
         if (isVimeoAppInstalled())
         {
-            let endpoint = VimeoApp.Upload
+            let endpoint = VimeoApp.Category(categoryURI)
             let URL = endpoint.URL
             
             return UIApplication.sharedApplication().openURL(URL)
@@ -112,12 +129,90 @@ public class VimeoAppClient
         
         return false
     }
-
+    
+    public class func showChannel(channelURI: String!) -> Bool
+    {
+        if (isVimeoAppInstalled())
+        {
+            let endpoint = VimeoApp.Channel(channelURI)
+            let URL = endpoint.URL
+            
+            return UIApplication.sharedApplication().openURL(URL)
+        }
+        
+        return false
+    }
+    
+    public class func showAllCategories() -> Bool
+    {
+        if (isVimeoAppInstalled())
+        {
+            let endpoint = VimeoApp.AllCategories
+            let URL = endpoint.URL
+            
+            return UIApplication.sharedApplication().openURL(URL)
+        }
+        
+        return false
+    }
+    
+    public class func showExplore() -> Bool
+    {
+        if (isVimeoAppInstalled())
+        {
+            let endpoint = VimeoApp.Explore
+            let URL = endpoint.URL
+            
+            return UIApplication.sharedApplication().openURL(URL)
+        }
+        
+        return false
+    }
+    
+    public class func showFeed() -> Bool
+    {
+        if (isVimeoAppInstalled())
+        {
+            let endpoint = VimeoApp.Feed
+            let URL = endpoint.URL
+            
+            return UIApplication.sharedApplication().openURL(URL)
+        }
+        
+        return false
+    }
+    
     public class func showMyProfile() -> Bool
     {
         if (isVimeoAppInstalled())
         {
             let endpoint = VimeoApp.Me
+            let URL = endpoint.URL
+            
+            return UIApplication.sharedApplication().openURL(URL)
+        }
+        
+        return false
+    }
+    
+    public class func showPlaylists() -> Bool
+    {
+        if (isVimeoAppInstalled())
+        {
+            let endpoint = VimeoApp.Playlists
+            let URL = endpoint.URL
+            
+            return UIApplication.sharedApplication().openURL(URL)
+        }
+        
+        return false
+    }
+    
+    public class func showUpload() -> Bool
+    {
+        if (isVimeoAppInstalled())
+        {
+            let endpoint = VimeoApp.Upload
             let URL = endpoint.URL
             
             return UIApplication.sharedApplication().openURL(URL)
